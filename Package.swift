67876bc816816b0ee5b8ea306476a728cf8852aa
Jason Loewy <jasonloewy@gmail.com>
2024-10-05 19:47:06 -0400

    Updated the package name.

--- Package.swift
+++ Package.swift
@@ -4,26 +4,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "ScrollSegmentControl",
+    name: "ScrollSegmentedControl",
     platforms: [.iOS("15.0")],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "ScrollSegmentControl",
-            targets: ["ScrollSegmentControl"]),
+            name: "ScrollSegmentedControl",
+            targets: ["ScrollSegmentedControl"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets are the basic building blocks of a` package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "ScrollSegmentControl",
+            name: "ScrollSegmentedControl",
             dependencies: []),
         .testTarget(
-            name: "ScrollSegmentControlTests",
-            dependencies: ["ScrollSegmentControl"]),
+            name: "ScrollSegmentedControlTests",
+            dependencies: ["ScrollSegmentedControl"]),
     ]
 )
